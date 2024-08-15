863e77cc25e3237c5f80d462a227f2563168cf43
CHEGEBB <chegephil24@gmail.com>
2024-08-15 21:28:16 +0300

    new

---
+++ 0x03-user_authentication_service/README.md
@@ -0,0 +1,7 @@
+# User Authentication Service
+
+## Overview
+
+This project involves creating a user authentication service using Flask, SQLAlchemy, and bcrypt for hashing passwords. The service includes creating user models, handling user sessions, and implementing secure password handling. The objective is to gain a thorough understanding of the mechanisms involved in user authentication by building the service from scratch.
+
+
---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+"""DB module.
+"""
+from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class.
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance.
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object.
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        '''Adds a user to the database
+        Args:
+            email: email of the user
+            hashed_password: hashed password of the user
+        Returns:
+            User: the created user
+        '''
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        '''
+        Finds a user by a given attribute
+        Args:
+            **kwargs: the attribute to search for
+        Returns:
+            User: the user found
+        '''
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        Update a user in the database
+        Args:
+            user_id: the user id
+            **kwargs: the attributes
+        Returns:
+            None
+        """
+        user = self.find_user_by(id=user_id)
+        if user is None:
+            return
+        update_source = {}
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                update_source[getattr(User, key)] = value
+            else:
+                raise ValueError()
+        self._session.query(User).filter(User.id == user_id).update(
+            update_source,
+            synchronize_session=False,
+        )
+        self._session.commit()
